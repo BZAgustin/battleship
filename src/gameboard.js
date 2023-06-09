17e4e62f3ceb62c729435b05960bc2e5d1fc543e
BZAgustin <abustoszanelli@gmail.com>
2023-06-09 12:00:37 -0300

    - Fix 'row' and 'column' misplacements - Add receiveAttack() and areShipsSunk()

--- src/gameboard.js
+++ src/gameboard.js
@@ -14,16 +14,16 @@ const gameboardFactory = () => {
     destroyer: shipFactory(2)
   }
 
-  function isOverlapping(ship, row, column, axis) {
-    if(axis === 0) {
+  function isOverlapping(ship, row, column, isVertical) {
+    if(isVertical === false) {
       for(let i = 0; i < ship.length; i+=1) {
-        if(board[column][row+i] !== null) {
+        if(board[row][column+i] !== null) {
           return true;
         }
       }
     } else {
       for(let i = 0; i < ship.length; i+=1) {
-        if(board[column+i][row] !== null) {
+        if(board[row+i][column] !== null) {
           return true;
         } 
       }
@@ -32,41 +32,56 @@ const gameboardFactory = () => {
     return false;
   }
 
-  function isOutOfBounds(ship, row, column, axis) {
-    if(axis === 0) {
-      return ship.length + row > 10;
+  function isOutOfBounds(ship, row, column, isVertical) {
+    if(isVertical === false) {
+      return ship.length + column > 10;
     }
     
-    return ship.length + column > 10;
+    return ship.length + row > 10;
   }
 
-  function placeShip(ship, row, column, axis) {
-    if(isOutOfBounds(ship, row, column, axis)) {
+  function placeShip(ship, row, column, isVertical) {
+    if(isOutOfBounds(ship, row, column, isVertical)) {
       throw new Error('Ship length exceeds boundaries');
-    } else if(isOverlapping(ship, row, column, axis)) {
+    } else if(isOverlapping(ship, row, column, isVertical)) {
       throw new Error('Overlapping other ship');
     }
 
-    if(axis === 0) {  
+    if(isVertical === false) {  
       for(let i = 0; i < ship.length; i+=1) {
-        board[column][row+i] = ship;
+        board[row][column+i] = ship;
       }
     } else {
       for(let i = 0; i < ship.length; i+=1) {
-        board[column+i][row] = ship;
+        board[row+i][column] = ship;
       }
     }
   }
 
   function receiveAttack(row, column) {
-    if(board[column][row] !== null) {
-      board[column][row].hit();
+    if(trackingBoard[row][column] === true) {
+      throw new Error('Coordinate already hit');
     }
 
-    trackingBoard[column][row] = 1;
+    if(board[row][column] !== null) {
+      board[row][column].hit();
+      board[row][column] = null;
+    }
+
+    trackingBoard[row][column] = true;
   }
 
-  return { board, float, placeShip, receiveAttack }
+  function areShipsSunk() {
+    for(let i = 0; i < 10; i+=1) {
+      if(!board[i].every(element => element === null)) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
+
+  return { board, trackingBoard, float, placeShip, receiveAttack, areShipsSunk }
 };
 
 export default gameboardFactory;
