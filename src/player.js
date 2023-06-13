0a734e15aef650e29b69de5148f1c15269769acb
BZAgustin <abustoszanelli@gmail.com>
2023-06-13 19:54:30 -0300

    Add Player object and respective tests

---
+++ src/player.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-restricted-syntax */
+import Gameboard from "./gameboard"
+import Ship from "./ship";
+
+const Player = () => {
+  const gameboard = Gameboard();
+  
+  const float = [
+    Ship(5),
+    Ship(4),
+    Ship(3),
+    Ship(3),
+    Ship(2)
+  ];
+
+  function isFloatSunk() {
+    Array.from(float).every((ship) => ship.isSunk() === true);
+  }
+
+  function placeMyFloat() {
+    const myFloat = [];
+    float.forEach(ship => myFloat.push(ship));
+
+    gameboard.placeFloat(myFloat);
+  }
+
+  return { gameboard, float, isFloatSunk, placeMyFloat };
+}
+
+export default Player;
---
+++ src/player.test.js
@@ -0,0 +1,54 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-undef */
+
+import Player from "./player";
+import Ship from "./ship";
+
+let player;
+let testboard;
+let ships = [];
+
+beforeEach(() => {
+  player = Player();
+  ships = [
+    Ship(5),
+    Ship(4),
+    Ship(3),
+    Ship(3),
+    Ship(2)
+  ];
+  testboard = Array.from({ length: 10 },
+            () => Array(10).fill(null));
+});
+
+test('Checks if a float is sunk', () => {
+  expect(JSON.stringify(player.gameboard.board)).toEqual(JSON.stringify(testboard));
+});
+
+test('Places the player float', () => {
+  testboard[0][0] = ships[0];
+  testboard[1][0] = ships[0];
+  testboard[2][0] = ships[0];
+  testboard[3][0] = ships[0];
+  testboard[4][0] = ships[0];
+
+  testboard[0][1] = ships[1];
+  testboard[1][1] = ships[1];
+  testboard[2][1] = ships[1];
+  testboard[3][1] = ships[1];
+
+  testboard[0][2] = ships[2];
+  testboard[1][2] = ships[2];
+  testboard[2][2] = ships[2];
+
+  testboard[0][3] = ships[3];
+  testboard[1][3] = ships[3];
+  testboard[2][3] = ships[3];
+
+  testboard[0][4] = ships[4];
+  testboard[1][4] = ships[4];
+
+  player.placeMyFloat();
+
+  expect(JSON.stringify(player.gameboard.board)).toEqual(JSON.stringify(testboard));
+});
